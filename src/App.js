b33d46f962bc530346a7ff306d3f800298b986b0
bahayg <bahayg@gmail.com>
2020-02-10 14:54:46 -0800

    Add semantic item to RecipeCard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MainPage from './components/MainPage'
 import MyPage from './components/MyPage'
@@ -29,9 +28,6 @@ class App extends Component  {
     .then(data => this.setState({allRecipes: data}))
   }
   
-
-
-
   
   render(){
     return (
@@ -47,55 +43,57 @@ class App extends Component  {
 
 export default App;
 
+  // ***** these methods get recipes form database and post to our database ***********
+            // fetchRecipes = () => {
+            //   fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=z')
+            //   .then(res => res.json())
+            //   .then(data => this.mapRecipes(data.meals))
+            // }
+          
+            // mapRecipes = recipes => {
+            //   recipes.map(recipe => {
+            //     this.postRecipe(recipe)
+            //   })
+            // }
+          
+            // postRecipe = recipe => {
+            //   fetch('http://localhost:3000/recipes', {
+            //     method: 'POST',
+            //     headers: {
+            //       "Content-Type" : 'application/json',
+            //       Accept: "application/json" 
+            //     },
+            //     body: JSON.stringify({
+            //       title: recipe.strMeal,
+            //       img: recipe.strMealThumb,
+            //       directions: recipe.strInstructions,
+            //       area: recipe.strArea,
+            //       category: recipe.strCategory,
+            //       ingredients: [
+            //         [recipe.strIngredient1, recipe.strMeasure1 ],
+            //         [recipe.strIngredient2, recipe.strMeasure2 ],
+            //         [recipe.strIngredient3, recipe.strMeasure3 ],
+            //         [recipe.strIngredient4, recipe.strMeasure4 ],
+            //         [recipe.strIngredient5, recipe.strMeasure5 ],
+            //         [recipe.strIngredient6, recipe.strMeasure6 ],
+            //         [recipe.strIngredient7, recipe.strMeasure7 ],
+            //         [recipe.strIngredient8, recipe.strMeasure8 ],
+            //         [recipe.strIngredient9, recipe.strMeasure9 ],
+            //         [recipe.strIngredient10, recipe.strMeasure10 ],
+            //         [recipe.strIngredient11, recipe.strMeasure11 ],
+            //         [recipe.strIngredient12, recipe.strMeasure12 ],
+            //         [recipe.strIngredient13, recipe.strMeasure13 ],
+            //         [recipe.strIngredient14, recipe.strMeasure14 ],
+            //         [recipe.strIngredient15, recipe.strMeasure15 ],
+            //         [recipe.strIngredient16, recipe.strMeasure16 ],
+            //         [recipe.strIngredient17, recipe.strMeasure17 ],
+            //         [recipe.strIngredient18, recipe.strMeasure18 ],
+            //         [recipe.strIngredient19, recipe.strMeasure19 ],
+            //         [recipe.strIngredient20, recipe.strMeasure20 ]
+            //       ],
+            //    })
+            //   })
+            // }
 
-////// ***** these methods get recipes form database and post to our database ***********
-          // fetchRecipes = () => {
-          //   fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=y')
-          //   .then(res => res.json())
-          //   .then(data => this.mapRecipes(data.meals))
-          // }
-        
-          // mapRecipes = recipes => {
-          //   recipes.map(recipe => {
-          //     this.postRecipe(recipe)
-          //   })
-          // }
-        
-          // postRecipe = recipe => {
-          //   fetch('http://localhost:3000/recipes', {
-          //     method: 'POST',
-          //     headers: {
-          //       "Content-Type" : 'application/json',
-          //       Accept: "application/json" 
-          //     },
-          //     body: JSON.stringify({
-          //       title: recipe.strMeal,
-          //       img: recipe.strMealThumb,
-          //       directions: recipe.strInstructions,
-          //       area: recipe.strArea,
-          //       category: recipe.strCategory,
-          //       ingredients: [
-          //         [recipe.strIngredient1, recipe.strMeasure1 ],
-          //         [recipe.strIngredient2, recipe.strMeasure2 ],
-          //         [recipe.strIngredient3, recipe.strMeasure3 ],
-          //         [recipe.strIngredient4, recipe.strMeasure4 ],
-          //         [recipe.strIngredient5, recipe.strMeasure5 ],
-          //         [recipe.strIngredient6, recipe.strMeasure6 ],
-          //         [recipe.strIngredient7, recipe.strMeasure7 ],
-          //         [recipe.strIngredient8, recipe.strMeasure8 ],
-          //         [recipe.strIngredient9, recipe.strMeasure9 ],
-          //         [recipe.strIngredient10, recipe.strMeasure10 ],
-          //         [recipe.strIngredient11, recipe.strMeasure11 ],
-          //         [recipe.strIngredient12, recipe.strMeasure12 ],
-          //         [recipe.strIngredient13, recipe.strMeasure13 ],
-          //         [recipe.strIngredient14, recipe.strMeasure14 ],
-          //         [recipe.strIngredient15, recipe.strMeasure15 ],
-          //         [recipe.strIngredient16, recipe.strMeasure16 ],
-          //         [recipe.strIngredient17, recipe.strMeasure17 ],
-          //         [recipe.strIngredient18, recipe.strMeasure18 ],
-          //         [recipe.strIngredient19, recipe.strMeasure19 ],
-          //         [recipe.strIngredient20, recipe.strMeasure20 ]
-          //       ],
-          //    })
-          //   })
-          // }
+
+
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Item } from 'semantic-ui-react'
 
 class RecipeCard extends Component {
 
@@ -12,14 +12,33 @@ class RecipeCard extends Component {
 // Signed in and its your own recipe 
 // button to edit/delete  
 
+showDetails = () => {
+console.log(this.props.recipe)
+let { recipe } = this.props;
+return (
+
+{/* <Item>
+    <Item.Header>{recipe.title}</Item.Header>
+    <Item.Image size='tiny' src={recipe.img} />
+    <Item.Content>
+        <Item.Meta>
+            <span className='area'>{recipe.area}</span>
+            <span className='category'>{recipe.category}</span>
+            <span className='rating'>{recipe.rating}</span>
+        </Item.Meta>
+        <Item.Description>{recipe.description}</Item.Description>
+    </Item.Content>
+</Item> */}
+
+)
+}
     render() {
         return (
-            <div >
+            <div onClick={() => this.showDetails()}>
                 {this.props.recipe.title}
             </div>
         )
     }
-
 }
 
 export default RecipeCard
