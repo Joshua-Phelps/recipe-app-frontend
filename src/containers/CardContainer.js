819ee80d3892cb8c9c20ae3e9f20ccfa37393820
github username <github email address>
2020-02-10 17:46:46 -0800

    Nav Bar updates

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import RecipeCard from '../components/RecipeCard'
 
-import { Card } from 'semantic-ui-react'
+import { Card, Container } from 'semantic-ui-react'
 
 
 // import { Grid } from 'semantic-ui-react'
@@ -13,7 +13,7 @@ class CardContainer extends Component {
     //// map over card and display them 
 
     renderCards = () => {
-        console.log(this.props.recipes)
+        // console.log(this.props.recipes)
         if (this.props.recipes !== undefined){
             return this.props.recipes.map(recipe => {
                 return (
@@ -28,9 +28,11 @@ class CardContainer extends Component {
     render() {
         return (
             <div>
-            <Card.Group itemsPerRow={3}>
-                    {this.renderCards()}
-            </Card.Group>
+                <Container>
+                    <Card.Group itemsPerRow={3}>
+                            {this.renderCards()}
+                    </Card.Group>
+                </Container>
 
             </div>
         )
